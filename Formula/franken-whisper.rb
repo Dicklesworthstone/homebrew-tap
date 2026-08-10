9c6e78306ef0e52b44f188715f776dad85a688d9
Jeff Emanuel <jeff141421@gmail.com>
2026-08-10 00:00:20 -0400

    fix franken-whisper formula audit metadata

--- Formula/franken-whisper.rb
+++ Formula/franken-whisper.rb
@@ -4,27 +4,26 @@
 class FrankenWhisper < Formula
   desc "Native Rust Whisper transcription and speaker diarization"
   homepage "https://github.com/Dicklesworthstone/franken_whisper"
-  version "0.7.1"
-  license "LicenseRef-MIT-OpenAI-Anthropic-Rider"
+  license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v#{version}/franken_whisper-#{version}-darwin_arm64.tar.gz"
+      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v0.7.1/franken_whisper-0.7.1-darwin_arm64.tar.gz"
       sha256 "27557d1d86848ec769b7a9bd7fe5b402474dc0abe22de1e657b1b5b24d615b37"
     end
     on_intel do
-      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v#{version}/franken_whisper-#{version}-darwin_amd64.tar.gz"
+      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v0.7.1/franken_whisper-0.7.1-darwin_amd64.tar.gz"
       sha256 "6259eb88afca96919fbdc91cb2821d198381ba16791871f0827376286eb946ff"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v#{version}/franken_whisper-#{version}-linux_amd64.tar.gz"
+      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v0.7.1/franken_whisper-0.7.1-linux_amd64.tar.gz"
       sha256 "ec1bbc0a58a556af0e2d05fd894531310f65641bd7da6db6eba80e13c6be5cc1"
     end
     on_arm do
-      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v#{version}/franken_whisper-#{version}-linux_arm64.tar.gz"
+      url "https://github.com/Dicklesworthstone/franken_whisper/releases/download/v0.7.1/franken_whisper-0.7.1-linux_arm64.tar.gz"
       sha256 "031626c887e08aa5710796466e69e7480a560184a66d6006f3bc41d2dc0e7be2"
     end
   end
